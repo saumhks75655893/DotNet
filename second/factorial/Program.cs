0474e5c11e178ca563ce379b8621e93bba68c58b
himanshukumar76 <himanshukumar75655893@gmail.com>
2024-12-27 11:22:45 +0530

    some new files and folders added and updated

---
+++ second/factorial/Program.cs
@@ -0,0 +1,9 @@
+﻿int x, factorial=1, num; 
+Console.WriteLine("Enter any number : "); 
+num = int.Parse(Console.ReadLine()); 
+
+for(x=1;x<=num; x++){
+    factorial = factorial * x; 
+}
+
+Console.Write("The Factorial is : " + num + " : " + factorial); 
